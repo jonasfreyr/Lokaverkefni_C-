7b8df24684ed19a78a218eb88a339df433eba3ef
Lárus A <larus@laruss-macbook-pro-3.local>
2018-12-04 20:37:06 +0000

    main2.cpp file bætt við

---
+++ main2.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lokaverk
+//
+//  Created by Lárus A. on 04/12/2018.
+//  Copyright © 2018 Lárus A. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
